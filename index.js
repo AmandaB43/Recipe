6422ba42fd2acb9db0acfce712ebff0bf69bbb85
Amanda <amandarb43@gmail.com>
2023-01-29 12:41:48 -0800

    added error GET method, tried to remove env and node_modules files

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-//GET Requests
+//GET METHOD:
+
+//home page
 app.get('/', function (req, res) {
    
     res.send(`
@@ -12,18 +14,26 @@ app.get('/', function (req, res) {
     </body>
     `)
 })
+//recipes page
 app.get('/Recipes', function (req, res) {
    
     res.send('Hello world')
 })
+//reviews page
 app.get('/Reviews', function (req, res) {
    
     res.send('Hello world')
 })
+//about page
 app.get('/About', function (req, res) {
    
     res.send('Hello world')
 })
+//error page
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
 
 // Listen for connections.
 app.listen(process.env.PORT)
